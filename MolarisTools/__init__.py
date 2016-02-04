6387d5c1d4cab214c666ffef0fcbab9b49d8bbd8
Mikolaj Feliks <mikolaj.feliks@gmail.com>
2016-02-03 17:27:12 -0800

    Added reader of FVX files.

--- MolarisTools/__init__.py
+++ MolarisTools/__init__.py
@@ -4,9 +4,7 @@
 # . Copyright : USC, Mikolaj Feliks (2016)
 # . License   : GNU GPL v3.0       (http://www.gnu.org/licenses/gpl-3.0.en.html)
 #-------------------------------------------------------------------------------
-from WHAMData            import WHAMTwoDistances, WHAMData
-from EVBMapping          import EVBMapping
-from CHELPGCharges       import CHELPGCharges
+# . QM/MM interface for Molaris
 from QMCallerMopac       import QMCallerMopac
 from QMCallerGaussian    import QMCallerGaussian
 
@@ -17,8 +15,14 @@ from MolarisInputFile    import MolarisInputFile
 from MolarisOutputFile   import MolarisOutputFile, MolarisOutputFile2
 from DistanceFile        import DistanceFile
 from AminoLibrary        import AminoAtom, AminoGroup, AminoComponent, AminoLibrary
+from FVXFile             import FVXFile
 
 # . Handling of other files
 from GaussianOutputFile  import GaussianOutputFile
 from MopacOutputFile     import MopacOutputFile
 from XYZTrajectory       import XYZTrajectory
+
+# . Miscellaneous
+from CHELPGCharges       import CHELPGCharges
+from EVBMapping          import EVBMapping
+from WHAMData            import WHAMTwoDistances, WHAMData
