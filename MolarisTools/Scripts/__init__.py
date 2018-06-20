3aee581e56cd359720e3112a3f391e1db9a8065b
Mikolaj Feliks <mikolaj.feliks@gmail.com>
2018-06-20 09:46:24 -0700

    Expose BondsFromDistances.

--- MolarisTools/Scripts/__init__.py
+++ MolarisTools/Scripts/__init__.py
@@ -4,7 +4,7 @@
 # . Copyright : USC, Mikolaj Feliks (2015-2018)
 # . License   : GNU GPL v3.0       (http://www.gnu.org/licenses/gpl-3.0.en.html)
 #-------------------------------------------------------------------------------
-from AminoComponents_FromPDB  import AminoComponents_FromPDB
+from AminoComponents_FromPDB  import AminoComponents_FromPDB, BondsFromDistances
 from CalculateLRA             import CalculateLRA, CalculateOneSidedLRA
 from DetermineBAT             import DetermineBAT
 from DetermineEVBParameters   import DetermineEVBParameters
